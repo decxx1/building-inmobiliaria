e666479e799d767a7704de2fa0c2bf5e4f0ba4d3
Damian Cisternas <decxx1@gmail.com>
2024-07-11 15:19:31 -0300

    call to action design

--- resources/js/Components/Web/CallToAction.vue
+++ resources/js/Components/Web/CallToAction.vue
@@ -1,13 +1,18 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+
+</script>
 <template>
     <section class="bg-white dark:bg-gray-900 py-12">
         <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
-            <div class="mx-auto max-w-screen-sm text-center">
-                <h2 class="mb-4 text-4xl tracking-tight font-extrabold leading-tight text-gray-900 dark:text-white">
-                    Start your free trial today</h2>
+            <div class="mx-auto max-w-screen-md text-center">
+                <h2 class="mb-4 text-4xl tracking-tight font-extrabold leading-tight text-primary-web-700 dark:text-white">
+                    ¿Quieres vender o alquilar una propiedad con nosotros?</h2>
                 <p class="mb-6 font-light text-gray-500 dark:text-gray-400 md:text-lg">
-                    Try Flowbite Platform for 30 days. No credit card required.</p>
-                <a href="#" class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
-                    Free trial for 30 days</a>
+                    Contáctenos para obtener más información.
+                </p>
+                <Link :href="route('contacto')" class="text-white bg-primary-web-700 hover:bg-primary-web-600 focus:ring-4 focus:ring-primary-web-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-primary-web-600 dark:hover:bg-primary-web-700 focus:outline-none dark:focus:ring-primary-web-800">
+                    Contáctenos</Link>
             </div>
         </div>
     </section>
--- resources/js/Components/Web/Features.vue
+++ resources/js/Components/Web/Features.vue
@@ -3,36 +3,36 @@ import { Link } from '@inertiajs/vue3';
 
 </script>
 <template>
-    <section class="bg-primary-web-700 dark:bg-gray-900 max-sm:pb-12 py-4 sm:py-12">
+    <section class="bg-primary-web-700 dark:bg-gray-800 max-sm:pb-12 py-4 sm:py-12">
         <div class="grid grid-cols-12 mx-auto">
             <div class="col-span-12 sm:col-span-6 lg:col-span-4 px-12 max-sm:pt-4 max-sm:pb-8 sm:py-4">
                 <img src="images/logos/building-inmobiliaria.webp" class="w-full min-[1420px]:w-9/12 mx-auto" alt="Building Inmobiliaria Logo" />
             </div>
             <div class="col-span-12 sm:col-span-6 lg:col-span-8 mx-auto my-auto">
                 <div class="grid grid-cols-2 min-[500px]:grid-cols-3 sm:grid-cols-2 lg:grid-cols-3 gap-2 lg:gap-12 items-center mx-auto">
-                    <Link :href="route('inmuebles',{'page':1, 'type':1})" class="bg-white dark:bg-gray-800 p-4 cursor-pointer transition-all transform hover:scale-105 hover:-rotate-6 rounded-2xl w-28 lg:w-36 h-28 lg:h-36 text-center">
+                    <Link :href="route('inmuebles',{'page':1, 'type':1})" class="bg-white p-4 cursor-pointer transition-all transform hover:scale-105 hover:-rotate-6 rounded-2xl w-28 lg:w-36 h-28 lg:h-36 text-center">
                         <img src="images/features/casas.svg" class="max-w-16 lg:max-w-24 mx-auto" alt="venta y alquiler de casas" />
-                        <h3 class="font-bold text-primary-web-700 dark:text-white">Casas</h3>
+                        <h3 class="font-bold text-primary-web-700 dark:text-black">Casas</h3>
                     </Link>
-                    <Link :href="route('inmuebles',{'page':1, 'type':2})" class="bg-white dark:bg-gray-800 p-4 cursor-pointer transition-all transform hover:scale-105 hover:-rotate-6  rounded-2xl w-28 lg:w-36 h-28 lg:h-36 text-center">
+                    <Link :href="route('inmuebles',{'page':1, 'type':2})" class="bg-white p-4 cursor-pointer transition-all transform hover:scale-105 hover:-rotate-6  rounded-2xl w-28 lg:w-36 h-28 lg:h-36 text-center">
                         <img src="images/features/deptos.svg" class="max-w-16 lg:max-w-24 mx-auto" alt="venta y alquiler de departamentos" />
-                        <h3 class="font-bold text-primary-web-700 dark:text-white">Deptos</h3>
+                        <h3 class="font-bold text-primary-web-700 dark:text-black">Deptos</h3>
                     </Link>
-                    <Link :href="route('inmuebles',{'page':1, 'type':32})" class="bg-white dark:bg-gray-800 p-4 cursor-pointer transition-all transform hover:scale-105 hover:-rotate-6  rounded-2xl w-28 lg:w-36 h-28 lg:h-36 text-center">
+                    <Link :href="route('inmuebles',{'page':1, 'type':32})" class="bg-white p-4 cursor-pointer transition-all transform hover:scale-105 hover:-rotate-6  rounded-2xl w-28 lg:w-36 h-28 lg:h-36 text-center">
                         <img src="images/features/terrenos.svg" class="max-w-16 lg:max-w-24 mx-auto" alt="venta y alquiler de terrenos" />
-                        <h3 class="font-bold text-primary-web-700 dark:text-white">Terrenos</h3>
+                        <h3 class="font-bold text-primary-web-700 dark:text-black">Terrenos</h3>
                     </Link>
-                    <Link :href="route('inmuebles',{'page':1, 'type':26})" class="bg-white dark:bg-gray-800 p-4 cursor-pointer transition-all transform hover:scale-105 hover:-rotate-6  rounded-2xl w-28 lg:w-36 h-28 lg:h-36 text-center">
+                    <Link :href="route('inmuebles',{'page':1, 'type':26})" class="bg-white p-4 cursor-pointer transition-all transform hover:scale-105 hover:-rotate-6  rounded-2xl w-28 lg:w-36 h-28 lg:h-36 text-center">
                         <img src="images/features/oficinas.svg" class="max-w-16 lg:max-w-24 mx-auto" alt="venta y alquiler de oficinas" />
-                        <h3 class="font-bold text-primary-web-700 dark:text-white">Oficinas</h3>
+                        <h3 class="font-bold text-primary-web-700 dark:text-black">Oficinas</h3>
                     </Link>
-                    <Link :href="route('inmuebles',{'page':1, 'type':22})" class="bg-white dark:bg-gray-800 p-4 cursor-pointer transition-all transform hover:scale-105 hover:-rotate-6  rounded-2xl w-28 lg:w-36 h-28 lg:h-36 text-center">
+                    <Link :href="route('inmuebles',{'page':1, 'type':22})" class="bg-white p-4 cursor-pointer transition-all transform hover:scale-105 hover:-rotate-6  rounded-2xl w-28 lg:w-36 h-28 lg:h-36 text-center">
                         <img src="images/features/comercios.svg" class="max-w-16 lg:max-w-24 mx-auto" alt="venta y alquiler de comercios" />
-                        <h3 class="font-bold text-primary-web-700 dark:text-white">Comercios</h3>
+                        <h3 class="font-bold text-primary-web-700 dark:text-black">Comercios</h3>
                     </Link>
-                    <Link :href="route('inmuebles',{'page':1, 'type':9})" class="bg-white dark:bg-gray-800 p-4 cursor-pointer transition-all transform hover:scale-105 hover:-rotate-6  rounded-2xl w-28 lg:w-36 h-28 lg:h-36 text-center">
+                    <Link :href="route('inmuebles',{'page':1, 'type':9})" class="bg-white p-4 cursor-pointer transition-all transform hover:scale-105 hover:-rotate-6  rounded-2xl w-28 lg:w-36 h-28 lg:h-36 text-center">
                         <img src="images/features/cocheras.svg" class="max-w-16 lg:max-w-24 mx-auto" alt="venta y alquiler de cocheras" />
-                        <h3 class="font-bold text-primary-web-700 dark:text-white">Cocheras</h3>
+                        <h3 class="font-bold text-primary-web-700 dark:text-black">Cocheras</h3>
                     </Link>
                 </div>
             </div>
