76c6380c393a37b5581c11b0fb08ba684122cac8
Damian Cisternas <decxx1@gmail.com>
2024-07-19 20:28:47 -0300

    minor changes

--- resources/js/Components/Web/Property.vue
+++ resources/js/Components/Web/Property.vue
@@ -148,11 +148,11 @@ const iconesProperty = () => {
 </script>
 <template>
     <article class="cursor-pointer w-64 min-[310px]:w-72 sm:w-80 h-max bg-white border border-primary-web-700 shadow hover:shadow-primary-web-700 hover:scale-105 transition-all duration-300 dark:bg-gray-700 dark:border-gray-700">
-        <Link :href="props.admin ? '' : route('inmuebles.show', {id: id})">
+        <Link :href="props.admin ? '' : route('inmuebles.show', {id: props.id})">
             <header>
                 <figure class="relative">
                     <span class="w-full h-56 block">
-                        <img class="w-full h-full object-cover" :src="imageCover ? imageCover : baseUrl + 'images/properties/sin-propiedad.svg'" alt="imagen de la propiedad" />
+                        <img class="w-full h-full object-cover" :src="props.imageCover ? props.imageCover : baseUrl + 'images/properties/sin-propiedad.svg'" alt="imagen de la propiedad" />
                         <div v-if="props.admin && !props.active" class="absolute right-0 top-0 h-16 w-16">
                             <div class="absolute rounded-tl-3xl rounded-tr-3xl transform rotate-45 bg-danger-600 text-center text-white font-bold py-1 right-[-19px] top-[35px] w-[140px]">
                                 Inactivo
@@ -160,7 +160,7 @@ const iconesProperty = () => {
                         </div>
                     </span>
                     <figcaption class="absolute text-lg text-white top-4">
-                        <p class="text-white bg-primary-web-700 px-4 font-medium dark:bg-gray-800">{{ status }}</p>
+                        <p class="text-white bg-primary-web-700 px-4 font-medium dark:bg-gray-800">{{ props.status }}</p>
                     </figcaption>
                     <figcaption class="absolute text-white -bottom-2 right-4 bg-primary-web-700 rounded px-2 pt-1 dark:bg-gray-800">
                         <span :class="['w-8 h-8', iconesProperty()]"></span>
@@ -168,8 +168,8 @@ const iconesProperty = () => {
                 </figure>
             </header>
             <div class="bg-primary-web-700 dark:bg-gray-800 py-2">
-                <div v-if="!priceHidden && price">
-                    <p :class="['text-white text-right font-extrabold px-3', sizeLetterClass(price)]">{{ isPriceDollar ? 'US$' : 'ARS$' }} {{ numberFormat(price) }}</p>
+                <div v-if="!props.priceHidden && props.price">
+                    <p :class="['text-white text-right font-extrabold px-3', sizeLetterClass(props.price)]">{{ props.isPriceDollar ? 'US$' : 'ARS$' }} {{ numberFormat(props.price) }}</p>
                 </div>
                 <div v-else>
                     <p class="text-white text-right font-extrabold px-3 text-2xl">Consultar</p>
@@ -177,36 +177,36 @@ const iconesProperty = () => {
 
             </div>
             <div class="p-2 text-left">
-                <h5 class="mb-2 text-xl font-bold tracking-tight text-primary-web-700 dark:text-white">{{ address }}</h5>
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-300 inline" v-html="formattedDescription(description)"></p>
-                <span v-if="description.length > 100"> (...)</span>
+                <h5 class="mb-2 text-xl font-bold tracking-tight text-primary-web-700 dark:text-white">{{ props.address }}</h5>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-300 inline" v-html="formattedDescription(props.description)"></p>
+                <span v-if="props.description.length > 100"> (...)</span>
             </div>
             <footer class="bg-primary-web-700 dark:bg-gray-800 py-2">
                 <div class="px-2 flex flex-wrap flex-row justify-start text-white">
-                    <div v-if="superficieTotal" class="flex flex-row items-center mr-3">
+                    <div v-if="props.superficieTotal" class="flex flex-row items-center mr-3">
                         <img class="w-6 mr-1" src="/images/icons-property/superficie.svg" alt="superficie total de la propiedad" />
-                        <span>{{ superficieTotal }} m2</span>
+                        <span>{{ props.superficieTotal }} m2</span>
                     </div>
-                    <div v-if="superficieCubierta" class="flex flex-row items-center mr-3">
+                    <div v-if="props.superficieCubierta" class="flex flex-row items-center mr-3">
                         <img class="w-5 mr-1" src="/images/icons-property/superficie_cubierta.svg" alt="superficie cubierta de la propiedad" />
-                        <span>{{ superficieCubierta }} m2</span>
+                        <span>{{ props.superficieCubierta }} m2</span>
                     </div>
-                    <div v-if="banos && banos !== 'Indistinto'" class="flex flex-row items-center mr-3">
+                    <div v-if="props.banos && props.banos !== 'Indistinto'" class="flex flex-row items-center mr-3">
                         <img class="w-5 mr-1" src="/images/icons-property/bano2.svg" alt="baños que tiene la propiedad" />
-                        <span>{{ banos }}</span>
+                        <span>{{ props.banos }}</span>
                     </div>
-                    <div v-if="dormitorios && dormitorios !== 'Indistinto'" class="flex flex-row items-center mr-3">
+                    <div v-if="props.dormitorios && props.dormitorios !== 'Indistinto'" class="flex flex-row items-center mr-3">
                         <img class="w-6 mr-1" src="/images/icons-property/dormitorio2.svg" alt="habitaciones que tiene la propiedad" />
-                        <span>{{ dormitorios }}</span>
+                        <span>{{ props.dormitorios }}</span>
                     </div>
-                    <div v-if="cochera && cochera !== 'Indistinto'" class="flex flex-row items-center mr-3">
+                    <div v-if="props.cochera && props.cochera !== 'Indistinto'" class="flex flex-row items-center mr-3">
                         <img class="w-6 mr-1" src="/images/icons-property/cochera.svg" alt="tiene cochera el inmueble" />
-                        <span>{{ cochera === 'Sin cochera' ? 'No' : 'Si' }}</span>
+                        <span>{{ props.cochera === 'Sin cochera' ? 'No' : 'Si' }}</span>
                     </div>
                 </div>
             </footer>
         </Link>
-            <div v-if="admin">
+            <div v-if="props.admin">
                 <div class="flex flex-row justify-between py-1">
                     <ButtonIcon
                         name="Editar"
